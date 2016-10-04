69d0db1a87dd560a9a9e967f3893ddbf4c725c1d
cypernaut <cornelius.griep@hu-berlin.de>
2016-10-05 00:48:40 +0200

    update newest

---
+++ php/para.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="de">
+<header>
+	<title>Fkt</title>
+</header> 
+<body>
+	<?php
+		$text = 'Arthur';
+		$suche = 'A';
+		echo stripos($text, $suche)
+	?>
+</body>
+</html>
